8aa45cd51737a40d9239d630998c60d877826c7e
vinothraj28 <removino68@gmail.com>
2017-06-30 12:18:37 +0530

    Add files via upload

---
+++ FirstRepeated.java
@@ -0,0 +1,31 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class FirstRepeated {
+	public static void main(String[] args){
+		int n;
+		int count=0;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the integers :");
+		n=s.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=s.nextInt();
+		}
+		for(int i=0;i<n;i++){
+			count=1;
+			for(int j=i+1;j<n;j++){
+				if(a[i]==a[j]){
+					count++;
+					a[j]='\0';
+				}
+			}if(count==2 && a[i]!=0){
+			System.out.println(a[i]);
+			break;
+		
+			}
+			}
+	}
+
+}
