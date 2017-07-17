e3f693c7d614608b73ac08fec0b06c4cb440da7a
vinothraj28 <removino68@gmail.com>
2017-07-17 12:22:40 +0530

    Update SumOFArray.java

--- SumOFArray.java
+++ SumOFArray.java
@@ -17,7 +17,7 @@ public class SumOFArray {
 			for( int j=i+1;j<n;j++){
 				for(int k=j+1;k<n;k++){
 					if(a[i]+a[j]==a[k]){
-						System.out.println(a[i]+"+"+a[j]+"+="+a[k]);
+						System.out.println(a[i]+"+"+a[j]+"="+a[k]);
 					}
 				}
 			}
